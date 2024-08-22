e16e9839792cb2cea0666a8b08427e7dbcdddeec
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-22 20:05:16 +0200

    add rest of the squares to the board

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,16 @@ function App() {
           <Square value={board[1]} chooseSquare={() => chooseSquare(1)} />
           <Square value={board[2]} chooseSquare={() => chooseSquare(2)} />
         </div>
-        <div className='row'></div>
-        <div className='row'></div>
+        <div className='row'>
+          <Square value={board[3]} chooseSquare={() => chooseSquare(3)} />
+          <Square value={board[4]} chooseSquare={() => chooseSquare(4)} />
+          <Square value={board[5]} chooseSquare={() => chooseSquare(5)} />
+        </div>
+        <div className='row'>
+          <Square value={board[6]} chooseSquare={() => chooseSquare(6)} />
+          <Square value={board[7]} chooseSquare={() => chooseSquare(7)} />
+          <Square value={board[8]} chooseSquare={() => chooseSquare(8)} />
+        </div>
       </section>
     </main>
   );
