d18cd68ff1499ff3f199f9ff14a480203c6db7cb
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-22 19:47:54 +0200

    initial commit + create game bord with styling

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
+  return (
+    <main>
+      <section className='board'>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
