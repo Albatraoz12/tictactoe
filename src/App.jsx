29e9c30f60502bebc7b68b4b7baa5b563f8ba14c
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-08-22 19:58:33 +0200

    display 3 squares and add value and function no logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable no-unused-vars */
 import './App.css';
 import { useState } from 'react';
+import Square from './components/Square';
 
 function App() {
+  //Map board 3x9
   const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
+
+  //Check winner or loser logic for the game
+  const chooseSquare = (squareIndex) => {};
+
   return (
     <main>
       <section className='board'>
-        <div className='row'></div>
+        <div className='row'>
+          <Square value={board[0]} chooseSquare={() => chooseSquare(0)} />
+          <Square value={board[1]} chooseSquare={() => chooseSquare(1)} />
+          <Square value={board[2]} chooseSquare={() => chooseSquare(2)} />
+        </div>
         <div className='row'></div>
         <div className='row'></div>
       </section>
